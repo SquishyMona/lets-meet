2d49745def5693ed6168361a43da1ab2b8ed5161
SquishyMona <kotonefemc@gmail.com>
2024-11-08 18:24:41 -0500

    analytics bug

--- app/lib/firebase/config.ts
+++ app/lib/firebase/config.ts
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-import { getAnalytics } from "firebase/analytics";
+import { getAnalytics, isSupported } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -22,4 +22,4 @@ const firebaseConfig = {
 export const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 export const auth = getAuth(app);
-const analytics = getAnalytics(app);
+const analytics = isSupported().then(supported => supported ? getAnalytics(app) : null);
