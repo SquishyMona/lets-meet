5524d3fb5c764c7623255ff6a7ba868ee0081225
SquishyMona <kotonefemc@gmail.com>
2024-11-08 17:51:43 -0500

    fixed type error

--- app/lib/firebase/auth.ts
+++ app/lib/firebase/auth.ts
@@ -1,7 +1,8 @@
 import { onAuthStateChanged as _onAuthStateChanged, GoogleAuthProvider, signInWithRedirect } from "firebase/auth";
 import { auth } from "@/app/lib/firebase/config";
+import { User } from "firebase/auth";
 
-export function onAuthStateChanged(cb) {
+export function onAuthStateChanged(cb: (user: User | null) => void) {
     return _onAuthStateChanged(auth, cb);
 }
 
