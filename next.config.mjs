6635b4bb8539e3bfb9105e629518d0d4762c9840
SquishyMona <kotonefemc@gmail.com>
2024-11-08 19:11:46 -0500

    fixing auth error

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    async rewrites() {
+        return [
+            {
+                source: '/api/__/handler',
+                destination: 'http://lets-meet-scheduling-app.firebaseapp.com/api/__/handler',
+            },
+        ];
+    }
+};
 
 export default nextConfig;
