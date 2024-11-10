f658c95b03149c3c25b87243ce6d0e529f6c4294
SquishyMona <kotonefemc@gmail.com>
2024-11-09 19:43:14 -0500

    trying again

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    async rewrites() {
-        return [
-            {
-                source: '/__/auth/:path*',
-                destination: 'http://lets-meet-scheduling-app.firebaseapp.com/__/auth/:path*',
-            },
-        ];
-    }
+
 };
 
 export default nextConfig;
